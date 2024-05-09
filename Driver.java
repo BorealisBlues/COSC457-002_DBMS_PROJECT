f1a061f87937e31859689d39436759d416bc710a
MAXDEL1001 <maxdeluca03@gmail.com>
2024-05-08 21:11:54 -0400

    make code look better

--- Driver.java
+++ Driver.java
@@ -62,7 +62,7 @@ public class Driver {
     public void createAccount() throws SQLException {
 
 
-        System.out.println("Already have an ccount? (yes/no)");
+        System.out.println("Already have an account? (yes/no)");
         String response = scanner.nextLine();
 
         if (response.equalsIgnoreCase("yes")) {
@@ -183,7 +183,6 @@ public class Driver {
 
     // display all the items of the selected shipper
     public void displayShipperItemsAndCreateShipment(String shipperId) throws SQLException {
-        // Insert a new Shipment record
         System.out.println("Enter the receiver ID for the new shipment:");
         int receiverId = scanner.nextInt();
         scanner.nextLine();  // Consume newline left-over
@@ -397,64 +396,6 @@ public class Driver {
 
         System.out.println("Shipment " + shipmentId + " has been delivered.");
     }
-
-
-
-
-    // public void displayAvailableShipmentsToCarrier() throws SQLException{
-    //     // Implimenting one of the expected queries
-    //     Statement stmt = con.createStatement(); // create a new statement object
-    //     ResultSet rs = stmt.executeQuery("SELECT * FROM Shipments WHERE 'pickup_time' IS NULL");
-    //     ResultSetMetaData rsmt = rs.getMetaData();
-    //     // this part lists all the results from the query, can be trimmed later for only relavent information
-    //     while(rs.next()){
-    //         for (int i = 1; i <= rsmt.getColumnCount(); i++){
-    //             if (i > 1) System.out.print(", ");
-    //             System.out.print(rs.getString(i) + " " + rsmt.getColumnName(i));
-    //         }
-    //         System.out.println("");
-    //     }
-    // }
-
-    // public String getShipperAddressByShipmentID(int shipmentID ) throws SQLException{
-    //     // the goal of this is to return the 
-    //     Statement stmt = con.createStatement(); // create a new statement object
-    //     ResultSet rs = stmt.executeQuery("SELECT addr FROM User WHERE user_id = " + shipmentID);
-    //     return rs.getString(0); // this will be the first response, no check to ensure its the only one
-    
-    // }
-
-    // public void addNewUser(String type, String name, String email, String phone) throws SQLException{
-    //     PreparedStatement pstmt = con.prepareStatement("INSERT INTO ? ('Name','Email','Phone') VALUES(?,?,?)");
-    //     pstmt.setString(0, "User");
-    //     pstmt.setString(1, name);
-    //     pstmt.setString(2, email);
-    //     pstmt.setString(3, phone);
-    //     pstmt.executeUpdate();
-    //     // checking to make sure that we recieve a valid type
-    //     switch(type.toLowerCase()){
-    //         case "reciever","carrier","shipper":
-    //         pstmt.setString(0, type.toLowerCase());
-    //         pstmt.executeUpdate();
-            
-    //         default:
-    //             System.out.println("Please try again and enter a valid user type! {reciever, carrier, shipper}");
-    //     }
-    // }
-
-    // public void addItemToShipperInventory(String shipper, String itemName, Float price) throws SQLException{
-    //     PreparedStatement findShipperID = con.prepareStatement(
-    //         "SELECT UID FROM SHIPPER WHERE 'name' = ?");
-    //     findShipperID.setString(1, shipper);
-    //     ResultSet rs = findShipperID.executeQuery();
-    //     int shipperID = rs.getInt("UID");
-    //     PreparedStatement addItem = con.prepareStatement(
-    //         "INSET INTO ITEM(shipped_by,'name',price) VALUES(?,?,?)");
-    //     addItem.setInt(1, shipperID);
-    //     addItem.setString(2, itemName);
-    //     addItem.setFloat(3, price);
-    //     addItem.executeUpdate();
-    // }
 
     
 }
